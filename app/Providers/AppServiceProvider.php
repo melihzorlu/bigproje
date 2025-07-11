d6187a9f613ae6414e27cf1cf09fb4ffeca53bd5
melihzorlu <melihhzorlu@gmail.com>
2025-07-12 01:52:44 +0300

    'dil icin tr ye gecildi ve blog footer icin duzenlendi'

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,8 +1,11 @@
 <?php
 
 namespace App\Providers;
-
+use Carbon\Carbon;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
+use App\Models\Blog;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,8 +20,13 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        App::setLocale('tr');
+        Carbon::setLocale('tr');
+
+        View::composer('*', function ($view) {
+            $view->with('latestBlogs', Blog::latest()->take(3)->get());
+        });
     }
 }
